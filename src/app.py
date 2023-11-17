6394c059268242f929131f0edfc8ee21a468113b
Telinc1 <telinc13@gmail.com>
2023-11-17 17:57:50 +0200

    Fix flake8 errors

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import socket
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return "Hello! This is a Flask application running on {}".format(socket.gethostname())
