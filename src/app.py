b9414e19b77cd17d33f95362ef0b46536eae58bd
Telinc1 <telinc13@gmail.com>
2023-11-17 17:54:29 +0200

    Example code change

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello! I am a Flask application running on {}".format(socket.gethostname())
+    return "Hello! This is a Flask application running on {}".format(socket.gethostname())
 
 
 if __name__ == '__main__':
